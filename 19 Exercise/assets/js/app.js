c48e732975df13ef990f9dda7f199294005c0794
-v <zawlinn.profile@outlook.com>
2025-04-28 11:57:22 +0700

    ECMA: Course Update

--- 19 Exercise/assets/js/app.js
+++ 19 Exercise/assets/js/app.js
@@ -1,24 +1,64 @@
 'use strict';
 
-// import {fullname} from './data.js';
-// import {fullname as fname} from './data.js';
-// import fullname from "./data.js";
-// import {birthYear as dob} from './data.js';
-// import {bio} from './data.js';
-// import {fname} from './data.js';
+import Book from "./data.js";
 
-// import "./data.js"; // // import script first and current script is last
-// import ('./data.js'); // current script first and import script is last
+class BookManage{
+    #bookObj;
+    #books = [];
+    #searchRes ;
+    
 
-// console.log('This is App Script!');
-// // console.log(fullname);
-// console.log(fname, dob, bio);
+    createBook(title,author,year){
+        this.#bookObj = new Book(title, author, year);
+        this.#books.push(this.#bookObj)
+    } 
+    
+    booklists(){
+        return this.#books.forEach((book,idx) => {
+           const {title, author, year} = book;
+            console.log(`${++idx} - ${this.#bookObj.bookinfo(title, author, year)}`);
+            
+        })
+    }
 
-import data from "./data.js";
+    getFilter(str){
+       if(typeof str == 'string'){
+        this.#searchRes = this.#books.filter((book)=> str.toLowerCase() == book.title.toLowerCase() || str.toLowerCase() == book.author.toLowerCase());
+
+        if(this.#searchRes){
+            console.log(`######## SEARCH RESEARCH 4 ${str.toUpperCase()} ########`);
+            this.#searchRes.forEach((data,idx) => {
+                const {title,author,year} = data;
+                console.log(`${++idx} - ${this.#bookObj.bookinfo(title, author, year)}`);
+            })
+        } 
+        
+        return -21;
+       }
+
+       return this.#searchRes;
+    }
+}
+
+const bookManageObj = new BookManage();
+
+// IMPORT BOOKs DATA
+
+bookManageObj.createBook('100 Ways to be Rich', 'John Matharew', 2010);
+bookManageObj.createBook('All About Money', 'John Matharew', 2012);
+bookManageObj.createBook('Money Psychology', 'Will Smith', 2008);
+bookManageObj.createBook('Communiction', 'Will Smith', 2010);
+bookManageObj.createBook('Rich Father, Poor Father', 'William Arthur', 2009);
+bookManageObj.createBook('Entreprenure Mind', 'William Arthur', 2008);
+bookManageObj.createBook('Rich Father, Poor Father', 'William Arthur', 2009);
+bookManageObj.createBook('How to Start A Business', 'William Arthur', 2010);
+bookManageObj.createBook('How to Make Inverstment Without Losing', 'William Arthur', 2012);
 
 
-console.log(data);
-// console.log(data[0]);
-// console.log(data[1]);
-// console.log(data[2]);
-console.log(data.fullname, data.age, data.job);
+// WORKING WITH BOOK MANAGER
+
+bookManageObj.booklists();
+
+console.log(bookManageObj.getFilter('William Arthur'));
+console.log(bookManageObj.getFilter('John Matharew'));
+// console.log(bookManageObj.getFilter(2010));
--- 19 Exercise/assets/js/data.js
+++ 19 Exercise/assets/js/data.js
@@ -4,10 +4,10 @@ export default class Book{
     constructor(title, author, year){
         this.title = title;
         this.author = author;
-        this.year = year
+        this.year = year;
     }
 
-    bookinfo(){
-        return `${this.title} is written ${this.author} in ${this.year}.`
+    bookinfo(title, author, year){
+        return `${title} is written ${author} in ${year}.`
     }
 }
